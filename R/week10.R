bd58ae5840793c123dcdf35e1caf65cb21459adc
Amanda Jensen <jens2482@umn.edu>
2024-03-30 09:48:40 -0500

    Data import and cleaning

--- R/week10.R
+++ R/week10.R
@@ -1,9 +1,17 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
+library(tidyverse)
+library(foreign)
+#library(haven)
 
 # Data Import and Cleaning
-
+data = read.spss("../data/GSS2016.sav", to.data.frame=TRUE) #looks like either the foreign library or haven are the main ways to import SPSS files, but for haven all the values were converted to numbers, so I went with foreign.
+#data2 = read_sav("../data/GSS2016.sav") #looks like either the foreign library or haven are the main ways to import SPSS files, but for haven all the values were converted to numbers, so I went with foreign.
+gss_tbl <- data %>%
+  drop_na(MOSTHRS) %>%
+  rename('work hours'= MOSTHRS) %>%
+  select (-c(HRS1, HRS2)) %>%
+  select(where(~mean(is.na(.)) < 0.75))
 
 # Visualization
 
